60bf24c8c32cd2b53c76994a01fe9231f0145134
RobinKocken <robin.a.kocken@gmail.com>
2023-07-02 18:13:17 +0200

    Stuff

--- Assets/Scripts/Base/BasePickUp.cs
+++ Assets/Scripts/Base/BasePickUp.cs
@@ -5,26 +5,27 @@ using UnityEngine;
 public class BasePickUp : MonoBehaviour
 {
     public InventoryManager inventoryManager;
-    public float distance;
+
+    public float sphereRadius;
+    public float rayDistance;
     public LayerMask mask;
 
     private void Update()
     {
-        if(Physics.BoxCast(transform.position, transform.localScale, transform.forward, out RaycastHit hit, transform.rotation, mask))
+        if(Physics.SphereCast(transform.position, sphereRadius, -transform.up, out RaycastHit itemHit, rayDistance, mask))
         {
-            Debug.Log("Hello");
-            if(hit.transform.gameObject.TryGetComponent<ItemPickUp>(out ItemPickUp itemPickUp))
+            if(itemHit.transform.gameObject.TryGetComponent<ItemPickUp>(out ItemPickUp itemPickUp))
             {
                 inventoryManager.AddItem(itemPickUp.item, itemPickUp.currentAmount, -1);
                 itemPickUp.DestroyItem();
             }
-            
         }
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawRay(transform.position, transform.forward * distance);
-        Gizmos.DrawWireCube(transform.position + transform.forward * distance, transform.localScale);
+        Gizmos.color = Color.red;
+        Debug.DrawRay(transform.position, -transform.up * rayDistance);
+        Gizmos.DrawWireSphere(transform.position + -transform.up * rayDistance, sphereRadius);
     }
 }
